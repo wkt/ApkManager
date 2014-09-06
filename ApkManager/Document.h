73bc22fd68aaba4490f35197e0bb0beaec3ecef4
wkt.MacOSX <weikting@gmail.com>
2014-09-06 21:19:37 +0800

    Initial Commit

---
+++ ApkManager/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  ApkManager
+//
+//  Created by WeiKeting on 09/06/2014.
+//  Copyright (c) 2014年 weiketing.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ ApkManager/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ApkManager
+//
+//  Created by WeiKeting on 09/06/2014.
+//  Copyright (c) 2014年 weiketing.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
